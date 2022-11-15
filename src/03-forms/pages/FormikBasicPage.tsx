546af844c2d7371cbaf064e225d59de5881ace5e
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-15 20:17:20 -0300

    manual validations

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,7 +1,37 @@
-import { useFormik } from "formik";
+import { useFormik, FormikErrors } from "formik";
 import "../styles/styles.css";
 
+interface FormValues {
+   firstName: string;
+   lastName: string;
+   email: string;
+}
+
 const FormikBasicPage = () => {
+   const validate = ({ firstName, lastName, email }: FormValues) => {
+      const errors: FormikErrors<FormValues> = {};
+
+      if (!firstName) {
+         errors.firstName = "Required";
+      } else if (firstName.length >= 15) {
+         errors.firstName = "Must be 15 characters or less";
+      }
+
+      if (!lastName) {
+         errors.firstName = "Required";
+      } else if (firstName.length >= 10) {
+         errors.firstName = "Must be 10 characters or less";
+      }
+
+      if (!email) {
+         errors.email = "Required";
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+         errors.email = "Invalid email address";
+      }
+
+      return errors;
+   };
+
    const { handleChange, values, handleSubmit } = useFormik({
       initialValues: {
          firstName: "Santiago",
@@ -11,6 +41,7 @@ const FormikBasicPage = () => {
       onSubmit: (values) => {
          console.log(values);
       },
+      validate,
    });
 
    return (
