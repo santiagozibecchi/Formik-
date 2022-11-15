7fcf7f12a1d74e7030d7c79b621b207f6ce5153b
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-15 19:56:54 -0300

    get input values with formik

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,20 +1,47 @@
+import { useFormik } from "formik";
 import "../styles/styles.css";
 
 const FormikBasicPage = () => {
+   const { handleChange, values, handleSubmit } = useFormik({
+      initialValues: {
+         firstName: "Santiago",
+         lastName: "",
+         email: "",
+      },
+      onSubmit: (values) => {
+         console.log(values);
+      },
+   });
+
    return (
       <div>
          <h1>Formik tutorial</h1>
-         <form noValidate>
+         <form onSubmit={handleSubmit} noValidate>
             <label htmlFor="firstName">First Name</label>
-            <input type="text" name="firstName" />
+            <input
+               onChange={handleChange}
+               type="text"
+               name="firstName"
+               value={values.firstName}
+            />
             <span>First Name is required</span>
 
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" name="lasttName" />
+            <input
+               onChange={handleChange}
+               type="text"
+               name="lastName"
+               value={values.lastName}
+            />
             <span>Last Name is required</span>
 
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input
+               onChange={handleChange}
+               type="email"
+               name="email"
+               value={values.email}
+            />
             <span>Email is required</span>
             <span>Check email</span>
 
