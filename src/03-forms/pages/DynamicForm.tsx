a8cd6740d45381ee42df0c57919ec0b538a75c35
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-16 18:43:18 -0300

    dynamic initialValues

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -2,21 +2,25 @@ import { Formik, Form } from "formik";
 import { MyTextInput } from "../components";
 import formJson from "../data/custom-form.json";
 
+const initialValues: { [key: string]: any } = {};
+
+for (const input of formJson) {
+   initialValues[input.name] = input.value;
+}
+
 export const DynamicForm = () => {
    return (
       <div>
          <h1>DynamicForm</h1>
 
          <Formik
-            initialValues={{
-               name: "",
-            }}
+            initialValues={initialValues}
             onSubmit={(values) => {
                console.log(values);
             }}
          >
             {(formik) => (
-               <Form>
+               <Form noValidate>
                   {formJson.map(({ type, name, placeholder, label }) => {
                      return (
                         <MyTextInput
