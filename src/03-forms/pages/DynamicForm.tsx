825767f789ce650f58660299dc9d280cd45a6ae9
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-16 19:00:42 -0300

    dynamic selects

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from "formik";
-import { MyTextInput } from "../components";
+import { MySelect, MyTextInput } from "../components";
 import formJson from "../data/custom-form.json";
 
 const initialValues: { [key: string]: any } = {};
@@ -21,17 +21,39 @@ export const DynamicForm = () => {
          >
             {(formik) => (
                <Form noValidate>
-                  {formJson.map(({ type, name, placeholder, label }) => {
-                     return (
-                        <MyTextInput
-                           key={name}
-                           label={label}
-                           name={name}
-                           type={type as any}
-                           placeholder={placeholder}
-                        />
-                     );
-                  })}
+                  {/* Basado en el type determino que tipo de control voy a regresar */}
+                  {formJson.map(
+                     ({ type, name, placeholder, label, options }) => {
+                        if (
+                           type === "input" ||
+                           type === "password" ||
+                           type === "email"
+                        ) {
+                           return (
+                              <MyTextInput
+                                 key={name}
+                                 label={label}
+                                 name={name}
+                                 type={type as any}
+                                 placeholder={placeholder}
+                              />
+                           );
+                        } else if (type === "select") {
+                           return (
+                              <MySelect key="name" label={label} name={name}>
+                                 <option value="">Selecciona una opción</option>
+                                 {options?.map(({ id, label }) => (
+                                    <option key={id} value={id}>
+                                       {label}
+                                    </option>
+                                 ))}
+                              </MySelect>
+                           );
+                        }
+
+                        throw new Error(`El type "${type}" no es soportado`);
+                     }
+                  )}
 
                   <button type="submit">Enviar</button>
                </Form>
